e79fabd92bab9c32dc3d754bf06a9c796a701d0a
licreyes <lic.reyescarlos@gmail.com>
2018-10-01 20:42:20 -0500

    se agregaron modificacions en index y somos se agrego php en corro.php

---
+++ correo.php
@@ -0,0 +1,12 @@
+<?php
+
+$destino="reyesprojects.service@gmail.com";
+$nombre= $_POST["nombre"];
+$corroe= $_POST["corroe"];
+$asunto= $_POST["asunto"];
+$mensaje= $_POST["mensaje"];
+$contenido="Nombre: ". $nombre ."\nCorreo: " . $corroe . "\nAsunto: " . $asunto . "\nMensaje: " . $mensaje;
+mail($destino,"Contacto",$contenido);
+header("location:index.html");
+
+?>
